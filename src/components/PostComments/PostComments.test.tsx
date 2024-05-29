a9875e69ea77c29c491f11f4dd6024984f1624a0
GTM35 <035guuh@gmail.com>
2024-05-29 16:22:50 -0300

    criando teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve realizar a inserção de dois comentários", () => {
+    render(<PostComment />);
+    const botao = screen.getByTestId("btn-comentar");
+    const campoComentario = screen.getByTestId("campo-comentario");
+
+    /* Comentário 1 */
+    fireEvent.change(campoComentario, {
+      target: { value: "Primeiro comentário" },
     });
+    fireEvent.click(botao); // Click no botão
+
+    /* Comentário 2 */
+    fireEvent.change(campoComentario, {
+      target: { value: "Segundo comentário" },
+    });
+    fireEvent.click(botao); //Click no botão
+  });
 });
